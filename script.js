b5ba5a098db6ede6d5377e14b9dce448269d8220
Aayushisingh936 <aayushisingh25@navgurukul.org>
2026-03-21 12:16:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function openquiz(){
+    window.location.href="quiz.html";
+}
+
+function openquote(){
+    window.location.href="quote.html";
+}
+
+function openTip(){
+    window.location.href="Tip.html";
+}
+
+function openlist(){
+    window.location.href="list.html";
+}
